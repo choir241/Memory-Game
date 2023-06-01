9fcd8f05e01bf523232074353af4f56372b3930b
Richard Choi <richardchoi54@gmail.com>
2023-06-01 09:01:21 -0400

    add timer functionality

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,45 @@ const fetchCardData = async() => {
         console.error(err);
     }
 }
+
+
+function startTimer(display) {
+    //hide start button to prevent restarting timer
+    document.querySelector("#start").classList.add("hidden");
+
+    //set timer to desired start time
+    let timer = 60 * .5;
+
+    //delays following function by one machine time millisecond
+    const intervalId = setInterval(() => {
+
+        //parseInt is to account for logic that combines minutes with 0 into string
+        let minutes = parseInt(timer / 60, 10);
+        let seconds = parseInt(timer % 60, 10);
+
+        //accounts for minutes/seconds specific format
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        //updates the timer display
+        display.textContent = minutes + ":" + seconds;
+
+        //logic for timer reaching below zero
+        if (--timer < 0) {
+            //stops timer
+            clearInterval(intervalId);
+            //shows start button to retry
+            document.querySelector("#start").classList.remove("hidden");
+            //notifies user they lost since timer ran out before win condition was met
+            alert("You Lost!");
+        }
+
+    }, 1000);
+}
+        
+    const display = document.querySelector('#time');
+
+    //starts timer
+    document.getElementById('start').addEventListener('click', () => { 
+		startTimer(display);
+    });     

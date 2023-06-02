f6051668de3a964dbcc19043ccde2b9c51ef784a
Richard Choi <richardchoi54@gmail.com>
2023-06-02 13:07:21 -0400

    add shuffle mechanic for cards

--- public/js/main.js
+++ public/js/main.js
@@ -2,18 +2,56 @@
 
 /* Public API for cards to fetch "https://memory-game-backend-pizi.onrender.com/api/cards" */
 
+const shuffleGameBoard = (data) => {
+    //have all the cards be included
+//no repeats
+//randomized each time
+
+const shuffleCards = (minRange, maxRange) => Math.floor(Math.random() * (maxRange-minRange +1) + minRange);
+
+let newSet = new Set();
+
+//this is 15 because Object.values transforms card data object into an array, and since array start at 0, the newSet.size needs to match the length of new array, aka 16
+while (newSet.size < 16){
+    newSet.add(shuffleCards(0, 15));
+}
+const randomizedIndices = [...newSet];
+const arrayOfCards = Object.values(data);
+
+const randomizedCards = randomizedIndices.map(index=>arrayOfCards[index]);
+
+for(let i = 0; i < randomizedCards.length; i++){
+    document.querySelector(`.card${i}`).classList.add(`${randomizedCards[i].color}`);
+}   
+}
+ 
 const fetchCardData = async() => {
     try{
-        const data = await fetch("https://memory-game-backend-pizi.onrender.com/api/cards");
-            data.then(res=>res.json());
-            data.then(data=>console.log(data));
+        fetch("https://memory-game-backend-pizi.onrender.com/api/cards")
+            .then(res=>res.json())
+            .then(data=>{
+                shuffleGameBoard(data);
+
+});
     }catch(err){
         console.error(err);
     }
 }
 
+fetchCardData();
 
+const startGame = () => {
+    localStorage.setItem("play", true);
+
+    const check1 = localStorage.getItem("play");
+    if(check1){
+
+    }
+}
+
+//timer
 function startTimer(display) {
+    startGame();
     //hide start button to prevent restarting timer
     document.querySelector("#start").classList.add("hidden");
 

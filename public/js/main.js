a3835b8218f45ee9ac71bd5d9f7994897a0dd1a4
chelseaerinavery <chelsea.erin.avery@gmail.com>
2023-06-10 10:09:14 -0600

    finished refactoring event handlers fix, added page refresh after win/lose

--- public/js/main.js
+++ public/js/main.js
@@ -47,15 +47,16 @@ function checkVictoryConditions(color1, color2, card1, card2) {
         winCondition = 0;
         //shows start button to retry
         document.querySelector("#start").classList.remove("hidden");
+        //refreshes page
+        location.reload(true);
     }
 
     else if (winCondition < 8 && color1 !== color2) {
         setTimeout(() => {
             //since cards don't match, hide both cards
-            document.querySelector(`.card${card1}`).classList.remove("selected");
-            document.querySelector(`.card${card2}`).classList.remove("selected");
+            firstCard.remove("selected");
+            secondCard.remove("selected");
         }, 500)
-
     }
 }
 
@@ -137,6 +138,8 @@ function startTimer(display) {
             document.querySelector("#start").classList.remove("hidden");
             //notifies user they lost since timer ran out before win condition was met
             alert("You Lost!");
+            //refreshes page
+            location.reload(true);
         }
     }, 1000);
 
